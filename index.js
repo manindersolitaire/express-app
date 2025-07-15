0b79fb220929aa2dc4756bed410c83905cbb98e7
maninder <manindersolitaire@gmail.com>
2025-07-15 16:20:15 +0530

    Backend Express Second Day

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const PORT = 3000
 
 // define
 
+app.use((req,res,next)=>{
+      console.log('A new request received at'+Date.now())
+      next()
+})
+
+
 app.get('/', (req,res)=>{
     res.send('Hello, Express')
 })
@@ -21,6 +27,29 @@ app.use('/search',router)
 //     res.send('This is a Contact Route')
 // })
 
+app.post('/users',express.json() ,(req,res)=>{
+    const {name, email} = req.body
+    res.json({
+        msg: `User ${name} with email ${email} created Successfully`
+    })
+})
+
+app.put('/users/:id',express.json() ,(req,res)=>{
+    const userId = req.params.id
+    const {name,email} = req.body
+
+    res.json({
+        msg : `User ${userId} updated ${name}, ${email}`
+    })
+
+})
+
+app.delete('/users/:id',express.json() ,(req,res)=>{
+        const userId = req.params.id
+        res.json({
+            msg : `User ${userId} deleted successfully`
+        })
+})
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
