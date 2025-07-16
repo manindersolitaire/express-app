b3909c225069946a67b4abc2cc7a10b0608afb67
maninder <manindersolitaire@gmail.com>
2025-07-16 16:26:40 +0530

    Backend Express Third Day

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import express from 'express'
 import { searchController, usernameController } from './controller.js'
 import router from './route.js'
+import multer from 'multer'
+
+const storage =  multer.diskStorage({
+    destination :'uploads',
+    filename : (req,file,cb) => {
+        cb(null,file.originalname)
+    }
+})
 
 const app = express()
-
+const upload = multer(
+    {
+    storage,
+    limits : {
+        fileSize : 1024000
+    }
+}
+)
 const PORT = 3000
 
-// define
+app.use(express.urlencoded({extended : true}))
+app.use(upload.single('image'))
+// set EJS as the view engine
+
+// app.set('view engine', 'ejs')
+
+// app.use('/public',express.static('public'))
+// app.use('/images',express.static('images'))
+
 
 app.use((req,res,next)=>{
       console.log('A new request received at'+Date.now())
@@ -15,7 +38,8 @@ app.use((req,res,next)=>{
 
 
 app.get('/', (req,res)=>{
-    res.send('Hello, Express')
+    const username =  'Maninder'
+    res.render('index', {username})
 })
 
 app.use('/user', router)
@@ -27,29 +51,37 @@ app.use('/search',router)
 //     res.send('This is a Contact Route')
 // })
 
-app.post('/users',express.json() ,(req,res)=>{
-    const {name, email} = req.body
-    res.json({
-        msg: `User ${name} with email ${email} created Successfully`
-    })
+// app.post('/users',express.json() ,(req,res)=>{
+//     const {name, email} = req.body
+//     res.json({
+//         msg: `User ${name} with email ${email} created Successfully`
+//     })
+// })
+
+// app.put('/users/:id',express.json() ,(req,res)=>{
+//     const userId = req.params.id
+//     const {name,email} = req.body
+
+//     res.json({
+//         msg : `User ${userId} updated ${name}, ${email}`
+//     })
+
+// })
+
+// app.delete('/users/:id',express.json() ,(req,res)=>{
+//         const userId = req.params.id
+//         res.json({
+//             msg : `User ${userId} deleted successfully`
+//         })
+// })
+
+app.post('/form', (req,res)=>{
+    console.log(req.body)
+    console.log(req.file)
+
+    res.send('Form Data Received..')
 })
 
-app.put('/users/:id',express.json() ,(req,res)=>{
-    const userId = req.params.id
-    const {name,email} = req.body
-
-    res.json({
-        msg : `User ${userId} updated ${name}, ${email}`
-    })
-
-})
-
-app.delete('/users/:id',express.json() ,(req,res)=>{
-        const userId = req.params.id
-        res.json({
-            msg : `User ${userId} deleted successfully`
-        })
-})
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
